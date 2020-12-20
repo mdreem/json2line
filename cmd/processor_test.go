a42078bdb204eafd74c3216dff61c58622d622e7
Marc Schiereck <mdreem@fastmail.fm>
2020-12-20 21:06:14 +0100

    add tests for templating

--- cmd/processor_test.go
+++ cmd/processor_test.go
@@ -5,6 +5,7 @@ import (
 	"io"
 	"strings"
 	"testing"
+	"text/template"
 )
 
 func TestProcessInput(t *testing.T) {
@@ -54,6 +55,70 @@ func TestProcessInput(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			w := &bytes.Buffer{}
 			err := ProcessInput(tt.args.r, w, nil)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("ProcessInput() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if gotW := w.String(); gotW != tt.wantW {
+				t.Errorf("ProcessInput() gotW = '%v', want '%v'", gotW, tt.wantW)
+			}
+		})
+	}
+}
+
+func TestProcessInputWithTemplate(t *testing.T) {
+	type args struct {
+		r io.Reader
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantW   string
+		wantErr bool
+	}{
+		{
+			name: "simple JSON",
+			args: args{
+				r: strings.NewReader("{\"key\": \"value\"}"),
+			},
+			wantW:   "->value<--><no value><-[<no value>-<no value>]\n",
+			wantErr: false,
+		},
+		{
+			name: "simple JSON with two elements",
+			args: args{
+				r: strings.NewReader("{\"key\": \"value\", \"anotherKey\": \"anotherValue\"}"),
+			},
+			wantW:   "->value<-->anotherValue<-[<no value>-<no value>]\n",
+			wantErr: false,
+		},
+		{
+			name: "nested JSON",
+			args: args{
+				r: strings.NewReader("{\"key\": \"value\", \"newLevel\" : {\"nestedKey1\": \"nestedValue1\", \"nestedKey2\": \"nestedValue2\" }}"),
+			},
+			wantW:   "->value<--><no value><-[nestedValue1-nestedValue2]\n",
+			wantErr: false,
+		},
+		{
+			name: "multiple lines of JSON",
+			args: args{
+				r: strings.NewReader("{\"key\": \"line1\"}\n{\"key\": \"line2\"}"),
+			},
+			wantW:   "->line1<--><no value><-[<no value>-<no value>]\n->line2<--><no value><-[<no value>-<no value>]\n",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			parse, err := template.New("test_template").Parse("->{{ .key }}<-->{{ .anotherKey }}<-[{{ .newLevel.nestedKey1 }}-{{ .newLevel.nestedKey2 }}]")
+			if err != nil {
+				t.Errorf("could not parse template: %v", err)
+				return
+			}
+
+			w := &bytes.Buffer{}
+			err = ProcessInput(tt.args.r, w, parse)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("ProcessInput() error = %v, wantErr %v", err, tt.wantErr)
 				return
