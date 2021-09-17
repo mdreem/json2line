d50b5f5927a45a234f29567d19b34e403478e64d
Marc Schiereck <mdreem@fastmail.fm>
2021-09-17 23:08:40 +0200

    move loading of template to separate file

--- cmd/root.go
+++ cmd/root.go
@@ -6,10 +6,8 @@ import (
 	"github.com/mdreem/json2line/configuration/load"
 	"github.com/mdreem/json2line/processor"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 	"os"
 	"strconv"
-	"strings"
 	"text/template"
 )
 
@@ -89,59 +87,6 @@ func handleVersionFlag(c *cobra.Command) bool {
 		return true
 	}
 	return false
-}
-
-func loadTemplate(rootCmd *cobra.Command) *template.Template {
-	formatter, err := rootCmd.Flags().GetString("formatter")
-	if err != nil {
-		common.PrintInformationf("could not fetch formatter option: %v\n", err)
-		os.Exit(1)
-	}
-	formattingTemplate := getFormatter(formatter)
-	return formattingTemplate
-}
-
-func getFormatter(formatter string) *template.Template {
-	templates := viper.GetStringMapString("templates")
-	formatString := templates[formatter]
-	if formatString != "" {
-		return createTemplate(formatter, formatString)
-	}
-	common.PrintInformationf("no formatter with the name '%s' defined\n", formatter)
-	return nil
-}
-
-func loadReplacements(rootCmd *cobra.Command) map[string]string {
-	configuredReplacements := viper.GetStringMapString("replacements")
-	adhocReplacements, err := rootCmd.Flags().GetStringArray("replacement")
-	if err != nil {
-		common.PrintInformationf("could not fetch replacement options: %v\n", err)
-		os.Exit(1)
-	}
-
-	var replacements = make(map[string]string)
-	for k, v := range configuredReplacements {
-		replacements[k] = v
-	}
-
-	for _, r := range adhocReplacements {
-		replacement := strings.Fields(r)
-		if len(replacement) != 2 {
-			common.PrintInformationf("replacement is not of correct format '<FROM> <TO>' where both values are separated via whitespace")
-		}
-
-		replacements[replacement[0]] = replacement[1]
-	}
-	return replacements
-}
-
-func createTemplate(formatter string, formatString string) *template.Template {
-	parse, err := template.New(formatter).Parse(formatString)
-	if err != nil {
-		common.PrintInformationf("could not parse template: %\n", err)
-		os.Exit(1)
-	}
-	return parse
 }
 
 func isInputFromPipe() bool {
